96786fd668ef9f988e28b7d70cdf362d50bd3326
Suman Kagithala <kagithalasuman1030@gmail.com>
2025-07-06 13:38:41 +0530

    fixed login

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -1,4 +1,4 @@
 
-export const Api_Path='http://localhost:4000'
+//export const Api_Path='http://localhost:4000'
 
-//export const Api_Path='https://doctor-ai-backend-5v3v.onrender.com'
+export const Api_Path='https://doctor-ai-backend-5v3v.onrender.com'
